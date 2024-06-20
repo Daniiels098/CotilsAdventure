920f91b2445bf46917b968dd62f2b939259fabef
Daniels098 <cl201117@g.unicamp.br>
2024-06-20 14:33:21 -0300

    Subindo arquivos rapidamente

--- Scripts/Scene_manager.gd
+++ Scripts/Scene_manager.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
-var next_scene = "VehicleWheel3D"
+@onready var next_scene = load("res://scenes/Salas_Aula/sala_azul.tscn")
 
 func _ready():
 	pass
 
 func transitionToScene(new_scene: String):
-	next_scene = new_scene
+	#next_scene = new_scene
 	$ScreenTransition/AnimationPlayer.play("FadeToBlack")
 
 func finished_fading():
+	var instance = next_scene.instantiate()
 	$CurrentScene.get_child(0).queue_free()
-	$CurrentScene.add_child(next_scene)
+	$CurrentScene.add_child(instance)
 	$ScreenTransition/AnimationPlayer.play("FadeToNormal")
